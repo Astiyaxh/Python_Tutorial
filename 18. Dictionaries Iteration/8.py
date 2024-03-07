a6a6db2f9a3f78128b1ed9b9a32e75e3d36bd859
Astiyaxh <ariyan.mehri00@gmail.com>
2024-03-07 21:41:11 +0330

    complexity of dict and combining it with list!

---
+++ 18. Dictionaries Iteration/8.py
@@ -0,0 +1,20 @@
+complexity = [
+    {
+        "hello": True,
+        "world": False
+    },
+    {
+        11.0: ["Temp", "Apple"],
+        37.54: ["Attack of angle", "Orange"],
+        23.44: ["Wind speed", "Mango"]
+    },
+    {
+        "Coding skills": True,
+        "Python skills": True
+    },
+    {
+        2212.0: "Cost",
+        372.11: "Code",
+        12.38: "Serial number"
+    }
+]
